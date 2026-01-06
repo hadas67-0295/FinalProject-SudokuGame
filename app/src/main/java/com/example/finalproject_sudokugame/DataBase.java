2fbb23fbf36a64cfbc7207b86f1175bca3433cfc
hadas <hadasabu67@gmail.com>
2026-01-06 18:03:58 +0200

    Add Room Database class for StatisticsEntity
    Created abstract DataBase class extending RoomDatabase
    Included abstract method for StatisticsDao
    Implemented singleton pattern with getInstance() for centralized database access
    Configured fallbackToDestructiveMigration for version management

---
+++ app/src/main/java/com/example/finalproject_sudokugame/DataBase.java
@@ -0,0 +1,24 @@
+package com.example.finalproject_sudokugame;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import android.content.Context;
+
+@Database(entities = {StatisticsEntity.class}, version = 1)
+public abstract class DataBase extends RoomDatabase {
+
+    public abstract StatisticsDao statisticsDao();
+
+    private static DataBase instance;
+
+    public static synchronized DataBase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context.getApplicationContext(),
+                            DataBase.class, "sudoku_database")
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+        return instance;
+    }
+}
